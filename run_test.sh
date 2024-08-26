913dd11fb660fc883c9cc266f8782e5c41595da3
Roman Romanov <romati1880@gmail.com>
2024-08-26 13:15:45 +0300

    Настроил alembic, для использования 2-х БД

--- run_test.sh
+++ run_test.sh
@@ -2,6 +2,6 @@
 
 export ENV=development
 
-cd /media/D/cnc_base_test
+cd /media/D/cnc_base_dev
 source venv_dev/bin/activate
 uvicorn app.main:app --host 0.0.0.0 --port 8343 --ssl-keyfile key.pem --ssl-certfile cert.pem
